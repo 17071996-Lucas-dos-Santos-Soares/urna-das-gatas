43a20a2ec0bc23eab4de17dc3f923fed3eea5011
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2026-02-07 22:21:05 -0300

    urna das gatas

---
+++ src/js/etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+  {
+    titulo: "Escolha suas gatas",
+    numeros: 4,
+    candidatos: [
+      {
+        numero: "3811",
+        nome: "Angela White",
+        partido: "Porn-hub",
+        fotos: [{ url: "angela.jpg", legenda: "Primeira Gata" }],
+      },
+      {
+        numero: "7722",
+        nome: "Lana Rhoades",
+        partido: "X-Videos",
+        fotos: [{ url: "lana.jpg", legenda: "Segunda Gata" }],
+      },
+    ],
+  },
+  {
+    titulo: "babes",
+    numeros: 3,
+    candidatos: [
+      {
+        numero: "934",
+        nome: "Riley reid",
+        partido: "Babes",
+        fotos: [{ url: "riley.jpg", legenda: "Gatinhas" }],
+      },
+      {
+        numero: "111",
+        nome: "Keisha Gray",
+        partido: "Sexy",
+        fotos: [{ url: "keisha.jpg", legenda: "Gatas" }],
+      },
+    ],
+  },
+  {
+    titulo: "Top",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "15",
+        nome: "Gia Derza",
+        partido: "Super gata",
+        fotos: [
+          { url: "gia.jpg", legenda: "Gostosa" },
+        ],
+      },
+      {
+        numero: "99",
+        nome: "Luna Star",
+        partido: "lindas",
+        fotos: [
+          { url: "luna-star.png", legenda: "tesão", small: true },
+        ],
+      },
+    ],
+  },
+];
